7d3c1897756f3f51236abbd3d6c06de97364f0ed
Vyacheslav Kovalev <vyacheslav.kovalev@phystech.edu>
2017-09-24 17:53:05 +0300

    task 1 done

--- httpserver/server/run.py
+++ httpserver/server/run.py
@@ -1,27 +1,60 @@
 # -*- coding: utf-8 -*-
 import socket
+import os
 
 
 def get_response(request):
-
-    return 'WRITE YOU RESPONSE HERE\n'
+    if (request == ''):
+        return ''
+    strokes = request.split('\r\n')
+    command = strokes[0].split()[0]
+    path = strokes[0].split()[1]
+    request_version = strokes[0].split()[2]
+    status = '200 OK'
+    text = ''
+    header = dict()
+    strokes = strokes[1:]
+    for str in strokes:
+        if (len(str.split(':')) > 1):
+            header[str.split(':')[0]] = str.split(':')[1]
+    if (command != 'GET'):
+        status = 'BAD'
+    if (path == '/'):
+        text = 'Hello mister!\nYou are: ' + header['User-Agent']
+    elif (path.split('/')[1] == 'media'):
+        if (path == '/media/' or path == '/media'):
+            text  = '<br>'.join(os.listdir('files'))
+        else:
+            try:
+                file = open('./files/' + path.split('/')[2], 'r')
+                text = file.read()
+            except IOError:
+                text = 'File not found'
+                status = '404 Not Found'
+    elif (path == '/test' or path == '/test/'):
+        text = request.replace('\n', '<br>')
+    else:
+        status = '404 Not Found'
+        text = 'Page not found'
+    return request_version + ' ' + status + '\r\nContent-Type: text/html\r\n\r\n' + text
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 8000))  #
-server_socket.listen(0)  #
+server_socket.bind(('localhost', 8000))  # Привязка сокета к localhost:8000
+server_socket.listen(0)  # Готовность прянимать connect() от клиентов
 
 print 'Started'
 
-while 1:
+while True:
     try:
         (client_socket, address) = server_socket.accept()
-        print 'Got new client', client_socket.getsockname()  #
-        request_string = client_socket.recv(2048)  #
-        client_socket.send(get_response(request_string))  #
+        print 'Got new client', client_socket.getsockname()  # Вывод адреса клиента
+        request_string = client_socket.recv(2048)  # Получение 2048 байт от клиента
+        print request_string
+        client_socket.send(get_response(request_string))  # Отправка ответа от сервера
         client_socket.close()
-    except KeyboardInterrupt:  #
+    except KeyboardInterrupt:  # Обработка исключения
         print 'Stopped'
-        server_socket.close()  #
+        server_socket.close()  # Остановка операций сокетом сервера и освобождение ресурсов
         exit()
